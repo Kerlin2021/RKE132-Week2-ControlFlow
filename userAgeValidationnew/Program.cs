14bba8725136161081715a610b4be1054cc8ac40
Kerlin Pennula <Pennulakerlin@gmail.com>
2022-09-17 10:02:33 +0300

    add weektwo code

---
+++ userAgeValidationnew/Program.cs
@@ -0,0 +1,18 @@
+﻿//rakendus küsib kasutajal sisestada tema vanuse
+//kui kasutaja vanus on väiksem kui 13, siis rakendusest kuvatakse "You are too young to use Instagram"
+//muul juhul
+//rakenduses kuvatakse "Welcome to Instagram"
+
+Console.WriteLine("Enter your age:");
+
+int userAge = int.Parse(Console.ReadLine());//"13"- Heap, 13-stack
+
+
+if (userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
---
+++ userAgevalitation2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajalt sisestada tema vanus
+//kui kasutaja vanus on väikem kui 13, siis konsoolis kuvatakse:
+//"you are too young to use instagram"
+//muul juhul
+//konsoolis kuvatakse "Welcome to Instagram"
+
+Console.WriteLine("Enter your age:");
+
+int userAge =Int32.Parse(Console.ReadLine()); 
+
+if(userAge > 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are to young to use Instagram!");
+}
